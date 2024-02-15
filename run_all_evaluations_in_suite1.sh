a3ead8c7b8f14a0f9d2c051d0c2a05602f43784c
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-02-15 17:56:59 -0500

    bundleeeeeele

---
+++ run_all_evaluations_in_suite1.sh
@@ -0,0 +1,11 @@
+if [ "$1" = "none" ]; then
+    model_name="TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T" 
+else
+    model_name="$1"
+fi
+
+lm-eval --model hf --model_args pretrained=$model_name --tasks piqa --device cuda:0 --batch_size 8
+lm-eval --model hf --model_args pretrained=$model_name --tasks hellaswag --device cuda:0 --batch_size 8 
+lm-eval --model hf --model_args pretrained=$model_name --tasks winogrande --device cuda:0 --batch_size 8 
+lm-eval --model hf --model_args pretrained=$model_name --tasks mmlu --device cuda:0 --batch_size 8 
+lm-eval --model hf --model_args pretrained=$model_name --tasks mmlu --device cuda:0 --batch_size 8 --num_fewshot 5 
